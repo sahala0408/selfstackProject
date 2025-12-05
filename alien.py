b53d2ff9e95ba57e99e6bc37c63745e15d7da5c5
sahala0408 <sahalakp05@gmail.com>
2025-12-05 15:22:14 +0530

    Third commit

--- alien.py
+++ alien.py
@@ -1,5 +1,5 @@
-import pygame  # type: ignore
-from pygame.sprite import Sprite  #* type: ignore*
+import pygame
+from pygame.sprite import Sprite
 
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
@@ -11,7 +11,7 @@ class Alien(Sprite):
         self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load("images/alien.bmp")
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
@@ -24,9 +24,11 @@ class Alien(Sprite):
     def check_edges(self):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
-        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
 
     def update(self):
         """Move the alien right or left."""
-        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.x += (self.settings.alien_speed *
+                        self.settings.fleet_direction)
         self.rect.x = self.x
--- ship.py
+++ ship.py
@@ -1,17 +1,18 @@
-import pygame  # type: ignore
+import pygame
+from pygame.sprite import Sprite
 
-
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
@@ -20,9 +21,14 @@ class Ship:
         # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
 
-        # Movement flags; start with a ship that's not moving.
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
         """Update the ship's position based on movement flags."""
